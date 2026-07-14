27d13331a87e415d666a97eb5b8adf3a7996a8e1
Sanaz Keyvanloo <sanaz.keyvanloo1992@gmail.com>
2026-07-14 10:25:25 +0330

    remove extra files

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
-// Route::view('dashboard', 'dashboard')
-//     ->middleware(['auth', 'verified'])
-//     ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-require __DIR__.'/auth.php';
+Route::get('/dashboard', function () {
+    return redirect()->route('admin.dashboard');
+});
