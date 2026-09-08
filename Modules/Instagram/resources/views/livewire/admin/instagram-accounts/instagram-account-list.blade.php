e74d539896b935313e4bddeced45b9f7439e6e0a
Sanaz Keyvanloo <sanaz.keyvanloo1992@gmail.com>
2026-09-08 13:28:30 +0330

    add posts btn for admin in accounts list

--- Modules/Instagram/resources/views/livewire/admin/instagram-accounts/instagram-account-list.blade.php
+++ Modules/Instagram/resources/views/livewire/admin/instagram-accounts/instagram-account-list.blade.php
@@ -1,2 +1,3 @@
 <x-Instagram::instagram-accounts.instagram-accounts-table :title="__('instagram::attributes.instagram_accounts_list')"
-    :instagram-accounts="$instagramAccounts" :conversations-route-name="'admin.conversations.index'" />
+    :instagram-accounts="$instagramAccounts" :conversations-route-name="'admin.conversations.index'"
+    :instagram-posts-list-route-name="'admin.instagram_posts.index'" />
