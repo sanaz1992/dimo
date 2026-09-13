bd58af7d8fc28f139252cdfa972cc43b28748942
Sanaz Keyvanloo <sanaz.keyvanloo1992@gmail.com>
2026-09-13 12:58:52 +0330

    test

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 // comment for test
+// test// comment for test
+// test// comment for test
+// test// comment for test
+// test
 
 Route::get('/dashboard', function () {
     return redirect()->route('admin.dashboard');
